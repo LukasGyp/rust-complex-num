e3226f2b1d826f3c11f6fbd20b98744652e82335
Lukas <eku1gyps0phi1a@gmail.com>
2023-06-26 18:08:07 +0900

    complex number

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::ops::{Add, Sub, Mul, Div};
+
+#[derive(Debug)]
+struct Complex {
+  re: f64,
+  im: f64,
+}
+
+impl Complex {
+  fn abs(&self) -> f64 {
+    (self.re * self.re + self.im * self.im).sqrt()
+  }
+
+  fn conj(&self) -> Complex {
+    Complex {
+      re: self.re,
+      im: -self.im
+    }
+  }
+}
+
+impl Add<&Complex> for &Complex {
+  type Output = Complex;
+
+  fn add(self, other: &Complex) -> Complex {
+    Complex {
+      re: self.re + other.re,
+      im: self.im + other.im,
+    }
+  }
+}
+
+impl Sub<&Complex> for &Complex {
+  type Output = Complex;
+
+  fn sub(self, other: &Complex) -> Complex {
+    Complex {
+      re: self.re - other.re,
+      im: self.im - other.im,
+    }
+  }
+}
+
+impl Mul<&Complex> for &Complex {
+  type Output = Complex;
+
+  fn mul(self, other: &Complex) -> Complex {
+    Complex {
+      re: self.re * other.re - self.im * other.im,
+      im: self.re * other.im + self.im * other.re,
+    }
+  }
+}
+
+impl Div<&Complex> for &Complex {
+  type Output = Complex;
+
+  fn div(self, other: &Complex) -> Complex {
+    Complex {
+      re: (self.re * other.re + self.im * other.im) / (other.re * other.re + other.im * other.im),
+      im: (self.re * other.im - self.im * other.re) / (other.re * other.re + other.im * other.im),
+    }
+  }
+}
+
+fn main() {
+  let x = Complex {re: 6., im:2.};
+  let y = Complex {re: 3., im:1.};
+
+  println!("x+y: {:?}", &x+&y);
+  println!("x-y: {:?}", &x-&y);
+  println!("xy: {:?}", &x*&y);
+  println!("x/y: {:?}", &x/&y);
+  println!("|x|: {:?}", x.abs());
+  println!("conj(x): {:?}", x.conj());
+}
