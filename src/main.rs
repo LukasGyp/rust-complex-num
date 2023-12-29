488adccfb4a4464da879d503614ea6c19d1df161
Luke-OGURO <eku1gyps0phi1a@gmail.com>
2023-12-29 13:19:32 +0900

    input

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 mod complex;
 use complex::Complex;
 
+use std::io::stdin;
+
+fn input() -> f64{
+  let mut a = String::new();
+  stdin().read_line(&mut a).unwrap();
+  return a.trim().parse::<f64>().unwrap();
+}
+
 fn main() {
-  let x = Complex {re: 6., im:2.};
-  let y = Complex {re: 3., im:1.};
+  let (xre, xim, yre, yim);
+  xre = input();
+  xim = input();
+  yre = input();
+  yim = input();
+  let x = Complex {re: xre, im: xim};
+  let y = Complex {re: yre, im: yim};
 
   println!("x+y: {}", x+y);
   println!("x-y: {}", x-y);
